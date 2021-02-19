e5992f2bd5203cdf82ec02d4e0e49eef352e59da
cyoq <di9akoz@gmail.com>
2021-02-19 10:09:33 +0200

    added testing class

---
+++ sorting/src/dip107/Testing.java
@@ -0,0 +1,15 @@
+package dip107;
+
+interface Test {
+    static void test(int[] arr) {}
+}
+
+public class Testing {
+
+    public final int seed = 5614592;
+    public double[][][] timeResults;
+
+    Testing(Test[] sortingAlgorithms) {
+
+    }
+}
