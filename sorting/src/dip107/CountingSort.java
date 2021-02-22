ee0cd640c4515a52ccf4a02da0b57e7a6e2d9fa9
cyoq <di9akoz@gmail.com>
2021-02-22 19:19:29 +0200

    Revert "Add files via upload"
    This reverts commit 6c45d07b1fa1c30ae8391853d5145014aa0f1f14.

--- sorting/src/dip107/CountingSort.java
+++ sorting/src/dip107/CountingSort.java
@@ -1,36 +1,10 @@
+package dip107;
 
 public class CountingSort {
-	
-	public static void sort (int[] a) {
-		//long t0 = System.nanoTime();
-		int i, j, k, n = a.length;
-		int b[] = new int[n];
-		int equal[] = new int[n];
-		int less[] = new int[n];
-		
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
-				if (a[i] == a[j]) {
-					equal[i] = equal[i]+1;
-				}
-				else if (a[i] > a[j]) {
-					less[i] = less[i]+1;
-				}
-			}
-		}
-		
-		for (i = 0; i < n; i++) {
-			k = less[i];
-			for (j = 0; j < equal[i]; j++) {
-				b[k+j] = a[i];
-			}
-		}
-		
-		for (i = 0; i < n; i++) {
-			a[i] = b[i];
-		}
-		
-		//long t = System.nanoTime();
-		//return t - t0;
-	}
+
+    public static void sort(int[] arr) { }
+
+    public static void test(int[] arr) {
+        sort(arr);
+    }
 }
