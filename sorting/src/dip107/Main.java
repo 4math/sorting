de73fb15d50e2a500a70f68f6b1a20d4d48f371b
cyoq <di9akoz@gmail.com>
2021-02-22 19:15:42 +0200

    added a link

--- sorting/src/dip107/Main.java
+++ sorting/src/dip107/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
         // It is needed to run test several times, because JVM runs slow at first run
         // due to JIT
+        // https://stackoverflow.com/questions/20655963/java-benchmarking-why-is-the-second-loop-faster
         for (int i = 0; i < 3; i++) {
             tf.test();
         }
