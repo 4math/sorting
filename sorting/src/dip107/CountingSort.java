809c0c281bb8a86c2b0e1e89c7bdc121360a8b55
cyoq <di9akoz@gmail.com>
2021-02-22 19:19:25 +0200

    Revert "Add files via upload"
    This reverts commit 77321b1a3543e3f7ab3e43a44eed43b80835154b.

--- sorting/src/dip107/CountingSort.java
+++ sorting/src/dip107/CountingSort.java
@@ -1,70 +1,36 @@
-//tut 2 vida sortirovki:
-//1) sort
-//2) modifiedSort
-public class CountingSort {	
-	public static void sort (int[] a, int direction) {
+
+public class CountingSort {
+	
+	public static void sort (int[] a) {
+		//long t0 = System.nanoTime();
 		int i, j, k, n = a.length;
 		int b[] = new int[n];
 		int equal[] = new int[n];
-		int less[] = new int[n];		
+		int less[] = new int[n];
+		
 		for (i = 0; i < n; i++) {
 			for (j = 0; j < n; j++) {
 				if (a[i] == a[j]) {
-					equal[i]++;
+					equal[i] = equal[i]+1;
 				}
 				else if (a[i] > a[j]) {
-					less[i]++;
+					less[i] = less[i]+1;
 				}
 			}
 		}
+		
 		for (i = 0; i < n; i++) {
 			k = less[i];
 			for (j = 0; j < equal[i]; j++) {
 				b[k+j] = a[i];
 			}
 		}
-		if (direction == 1) {
-			for (i = 0; i < n; i++) {
-				a[i] = b[i];
-			}
+		
+		for (i = 0; i < n; i++) {
+			a[i] = b[i];
 		}
-		else {
-			for (i = 0; i < n; i++) {
-				a[n-i-1] = b[i];
-			}
-		}
-	}
-	public static void modifiedSort(int[] a, int direction) {
-		int m = Integer.MAX_VALUE, n = Integer.MIN_VALUE;
-		for (int i = 0; i < a.length; i++) {
-			if (m > a[i]) {
-				m = a[i];
-			}
-			if (n < a[i]) {
-				n = a[i];
-			}
-		}
-		int[] extra = new int[n-m+1];
-		for (int i = 0; i < a.length; i++) {
-			extra[a[i] - m]++;
-		}
-		if (direction == 1) {
-			int q = 0;
-			for (int i = 0; i < extra.length; i++) {
-				for (int j = 0; j < extra[i]; j++) {
-					a[q] = m + i;
-					q++;
-				}
-			}
-		}
-		else {
-			int q = a.length - 1;
-			for (int i = 0; i < extra.length; i++) {
-				for (int j = 0; j < extra[i]; j++) {
-					a[q] = m + i;
-					q--;
-				}
-			}
-		}
+		
+		//long t = System.nanoTime();
+		//return t - t0;
 	}
 }
